3d58b5bf7b8f9183e82abd0b66aca89d0c7a82de
johndward01 <johndward01@gmail.com>
2023-02-25 19:57:18 -0600

    Add project files.

---
+++ Interfaces_Exercise/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Interfaces_Exercise
+{
+    public interface IPlayable
+    {
+        public string Platform { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public void Play();
+        public void Quit();
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: Create classes that will conform to IPlayable (ex: PlayStation, Xbox, Switch, Steam)
+            // TODO: Instantiate each class and call it's Play() and Quit() methods (hint: they can just write out "Playing..." or "Quitting..." to the console)
+            
+        }
+    }
+}
